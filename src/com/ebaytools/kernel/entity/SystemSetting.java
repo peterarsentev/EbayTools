46bf40691796ea7f86af35781f2d173cc4772347
Peter Arsentev <parsentev@yandex.ru>
2012-01-07 14:11:30 +0300

    add save choose options

---
+++ src/com/ebaytools/kernel/entity/SystemSetting.java
@@ -0,0 +1,55 @@
+package com.ebaytools.kernel.entity;
+
+import java.io.Serializable;
+
+public class SystemSetting implements Serializable {
+    private Long id;
+    private String name;
+    private String value;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        SystemSetting that = (SystemSetting) o;
+
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (value != null ? !value.equals(that.value) : that.value != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (value != null ? value.hashCode() : 0);
+        return result;
+    }
+}
