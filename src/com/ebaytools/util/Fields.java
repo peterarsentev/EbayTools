0fc8950a874818b207899657ed09e027d686b969
Peter Arsentev <parsentev@yandex.ru>
2012-01-30 18:26:45 +0300

    changed average price. to calculate together for all day.

--- src/com/ebaytools/util/Fields.java
+++ src/com/ebaytools/util/Fields.java
@@ -1,14 +1,21 @@
 package com.ebaytools.util;
 
 public enum Fields {
-    AUCTION_PRICE("auction_price", "auction price"),
+    //filters fields
     IS_GOLDEN_FILTER_FIELD("is_golden", "is golden"),
+    APPLY_FILTER("apply_filter", "apply_filter"),
+    CONDITIONS("conditions", "conditions"),
+    TIME_OF_DAY("time_of_day", "time_of_day"),
+    DATE_CLOSE_FROM("close_auction_date_from", "close_auction_date_from"),
+    DATE_CLOSE_TO("close_auction_date_to", "close_auction_date_from"),
+
+    //items fields
+    AUCTION_PRICE("auction_price", "auction price"),
     SHIPPING_COST("shipping_cost", "shipping cost"),
     TOTAL_COST("total_cost", "total cost"),
     AUCTION_STATUS("auction_status", "auction status"),
-    CONDITIONS("conditions", "conditions"),
-    TIME_OF_DAY("time_of_day", "time_of_day"),
-    APPLY_FILTER("apply_filter", "apply_filter"),
+
+    //systems fields
     CHOSE_OPTIONS("chose_options", "chose_optios");
 
 
