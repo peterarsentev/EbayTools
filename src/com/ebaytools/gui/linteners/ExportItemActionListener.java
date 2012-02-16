c2bedf60ee57477a3f94d96eb56683b08215430b
Peter Arsentev <parsentev@yandex.ru>
2012-02-16 08:42:17 +0300

    sold fixed from todo_list #6

--- src/com/ebaytools/gui/linteners/ExportItemActionListener.java
+++ src/com/ebaytools/gui/linteners/ExportItemActionListener.java
@@ -1,5 +1,6 @@
 package com.ebaytools.gui.linteners;
 
+import au.com.bytecode.opencsv.CSVWriter;
 import com.ebaytools.gui.model.Data;
 import com.ebaytools.kernel.dao.ManagerDAO;
 import com.ebaytools.kernel.entity.Item;
@@ -12,6 +13,8 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.StringWriter;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
@@ -28,34 +31,37 @@ public class ExportItemActionListener implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         List<Item> items = ManagerDAO.getInstance().getItemDAO().getAllItems();
-        StringBuilder sb = new StringBuilder();
-        sb.append("\"referenceid\";\"itemid\";\"golden\";\"closeDate\";\"closePrice\";\"condition\";\"listingType\";\"submitDate\";");
-        sb.append("\"sold\";\"totalCost\";\"ShippingCost\";\"AuctionStatus\";\"TotalBid\";\"PositiveFeedBackPercent\";\"FeedBackScore\";");
-        sb.append("\"TotalRateSeller\";\"ShipToLocation\";\"HandlingTime\";");
-        sb.append("\r\n");
+        List<String[]> datas = new ArrayList<String[]>();
+        datas.add(new String[] {"referenceid", "itemid", "golden", "closeDate", "closePrice", "condition", "listingType", "submitDate",
+        "sold", "totalCost", "ShippingCost", "AuctionStatus", "TotalBid", "PositiveFeedBackPercent", "FeedBackScore",
+        "TotalRateSeller", "ShipToLocation", "HandlingTime"});
         for (Item item : items) {
             Map<Fields, ItemProperties> values = Fields.buildProperties(item.getProperties());
-            sb.append("\"").append(item.getProduct().getReferenceId()).append("\"").append(";");
-            sb.append("\"").append(item.getEbayItemId()).append("\"").append(";");
-            sb.append("\"").append(item.getGolden()).append("\"").append(";");
-            sb.append("\"").append(FormatterText.dateformatter.format(item.getCloseDate().getTime())).append("\"").append(";");
-            sb.append("\"").append(values.get(Fields.AUCTION_PRICE).getValue()).append("\"").append(";");
-            sb.append("\"").append(values.get(Fields.CONDITIONS).getValue()).append("\"").append(";");
-            sb.append("\"").append(values.get(Fields.LISTING_TYPE) != null ? values.get(Fields.LISTING_TYPE).getValue() : "-").append("\"").append(";");
-            sb.append("\"").append(FormatterText.dateformatter.format(item.getCreateDate().getTime())).append("\"").append(";");
-            sb.append("\"").append(item.getNameStatus()).append("\"").append(";");
-            sb.append("\"").append(values.get(Fields.TOTAL_COST).getValue()).append("\"").append(";");
-            sb.append("\"").append(values.get(Fields.SHIPPING_COST).getValue()).append("\"").append(";");
-            sb.append("\"").append(values.get(Fields.AUCTION_STATUS).getValue()).append("\"").append(";");
-            sb.append("\"").append(item.getTotalBid()).append("\"").append(";");
-            sb.append("\"").append(values.get(Fields.POSITIVE_FEEDBACK_PERCENT) != null ? values.get(Fields.POSITIVE_FEEDBACK_PERCENT).getValue() : "-").append("\"").append(";");
-            sb.append("\"").append(values.get(Fields.FEEDBACK_SCORE) != null ? values.get(Fields.FEEDBACK_SCORE).getValue() : "-").append("\"").append(";");
-            sb.append("\"").append(values.get(Fields.SHIP_TO_LOCATION) != null ? values.get(Fields.SHIP_TO_LOCATION).getValue() : "-").append("\"").append(";");
-            sb.append("\"").append(values.get(Fields.HANDLING_TIME) != null ? values.get(Fields.HANDLING_TIME).getValue() : "-").append("\"").append(";");
-            sb.append("\r\n");
+            datas.add(new String[] {
+            item.getProduct().getReferenceId(),
+            item.getEbayItemId(),
+            String.valueOf(item.getGolden()),
+            FormatterText.dateformatter.format(item.getCloseDate().getTime()),
+            values.get(Fields.AUCTION_PRICE).getValue(),
+            values.get(Fields.CONDITIONS).getValue(),
+            values.get(Fields.LISTING_TYPE) != null ? values.get(Fields.LISTING_TYPE).getValue() : "-",
+            FormatterText.dateformatter.format(item.getCreateDate().getTime()),
+            item.getNameStatus(),
+            values.get(Fields.TOTAL_COST).getValue(),
+            values.get(Fields.SHIPPING_COST).getValue(),
+            values.get(Fields.AUCTION_STATUS).getValue(),
+            String.valueOf(item.getTotalBid()),
+            values.get(Fields.POSITIVE_FEEDBACK_PERCENT) != null ? values.get(Fields.POSITIVE_FEEDBACK_PERCENT).getValue() : "-",
+            values.get(Fields.FEEDBACK_SCORE) != null ? values.get(Fields.FEEDBACK_SCORE).getValue() : "-",
+            values.get(Fields.SHIP_TO_LOCATION) != null ? values.get(Fields.SHIP_TO_LOCATION).getValue() : "-",
+            values.get(Fields.HANDLING_TIME) != null ? values.get(Fields.HANDLING_TIME).getValue() : "-"
+            });
         }
+        StringWriter sw = new StringWriter();
+        CSVWriter writer = new CSVWriter(sw);
+        writer.writeAll(datas);
         File file = new File(Calendar.getInstance().getTimeInMillis()+".csv");
-        FileUtil.save(file, sb.toString());
+        FileUtil.save(file, sw.toString());
         data.getText().append("\nFile was saved successful part : " + file.getAbsolutePath());
     }
 }
